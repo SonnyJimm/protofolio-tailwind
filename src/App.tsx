9827b8d194c430cfa90510331cfeedc157ebfcf0
SonnyJim <newbey009@gmail.com>
2023-11-26 22:06:47 -0600

    some initial divs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 const App = () => {
   return (
-    <div className="h-screen w-screen bg-black lg:bg-gradient-to-r sm:bg-gradient-to-t sm:from-black sm:from-60% sm:via-violet-950 sm:to-violet-900 "></div>
+    <div className="h-screen w-screen flex flex-col-reverse text-white lg:flex-row  bg-black lg:bg-gradient-to-r sm:bg-gradient-to-t sm:from-black sm:from-60%  sm:to-violet-900 ">
+      <div className="basis-[95%] lg:basis-3/5 "></div>
+      <div className="basis-[5%] flex flex-row items-center lg:items-start lg:basis-2/5 gap-2 lg:flex-col lg:justify-center lg: ml-20">
+        <p>hello</p>
+        <p>hello as</p>
+        <p>hello asdasd</p>
+        <p>hello asdasdasd</p>
+      </div>
+    </div>
   );
 };
 
